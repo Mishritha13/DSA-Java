186c33b243fb44b3d82dd8f5f4ff736d1a42a6a6
Mishritha13 <mishrithapuppalaa@gmail.com>
2026-06-14 11:20:00 +0530

    Added solution for finding max consecutive ones
    Implement a method to find the maximum number of consecutive 1s in an array.

---
+++ Arrays/Max Consecutive Ones.java
@@ -0,0 +1,18 @@
+//Time Complexity: O(N) Space Complexity: O(1)
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxi = 0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                maxi = Math.max(maxi,count);
+            }
+            else{
+                count=0;
+            }
+        }
+        return maxi;
+        
+    }
+}
