c799643e2ac6052ecb4d481dc866f9a5031ec56a
Mishritha13 <mishrithapuppalaa@gmail.com>
2026-06-16 18:35:26 +0530

    Added sorting algorithms for sorting colors array
    Implemented two sorting algorithms for the colors problem: a brute force method with O(2n) time complexity and O(1) space complexity, and an optimal method with O(n) time complexity and O(1) space complexity.

---
+++ Arrays/Sort colors[0,1,2].java
@@ -0,0 +1,45 @@
+//brute force O(2n), sc = O(1)
+class Solution {
+    public void sortColors(int[] nums) {
+        int cnt0=0; 
+        int cnt1=0;
+        int cnt2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0) cnt0++;
+            else if(nums[i]==1) cnt1++;
+            else cnt2++;
+        }
+        for(int i=0;i<cnt0;i++) nums[i]=0;
+        for(int i=cnt0;i<cnt0+cnt1;i++) nums[i]=1;
+        for(int i=cnt0+cnt1;i<nums.length;i++) nums[i]=2;
+
+        
+    }
+    
+}
+
+// optimal sol'n TC = O(n), SC = O(1)
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0, mid =0, high =nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+
+            }
+            else{
+                int temp = nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
+    }
+}
