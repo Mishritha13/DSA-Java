6295abe27740bc86867333422e98854136e14194
Mishritha13 <mishrithapuppalaa@gmail.com>
2026-07-01 17:47:28 +0530

    Implemented maxSubArray method to find maximum sum
    Added method to find and print the maximum subarray sum and its elements.

--- Arrays/Maximum Subarray Sum.java
+++ Arrays/Maximum Subarray Sum.java
@@ -49,3 +49,32 @@ class Solution {
         return maxi;
     }
 }
+// To print the subarray elements 
+class Solution {
+    public static void maxSubArray(int[] nums) {
+        int sum = 0, maxi = Integer.MIN_VALUE;
+        int start = 0, end = 0, tempStart = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+                start = tempStart;
+                end = i;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+                tempStart = i + 1;
+            }
+        }
+
+        System.out.println("Maximum Sum = " + maxi);
+        System.out.print("Subarray: ");
+
+        for (int i = start; i <= end; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
